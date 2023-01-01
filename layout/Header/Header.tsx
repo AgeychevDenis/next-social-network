1712164553adc55c72a5c1191bc59af1718b33e7
Ageychev Denis <ageychevdenis@bk.ru>
2023-01-01 23:00:51 +0300

    Added layout header

---
+++ layout/Header/Header.tsx
@@ -0,0 +1,62 @@
+import React, { useState, MouseEvent } from 'react';
+import { HeaderProps } from './Header.props';
+import UserImage from '../../public/assets/img/users/header-user.jpg'
+import styles from './Header.module.scss';
+import SearchIcon from './search.svg';
+import LogoIcon from './logo.svg';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+type PopupClick = MouseEvent & {
+   path: Node[];
+}
+
+const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
+   const [activeIndex, setActiveIndex] = useState(0);
+
+   const onClickCategory = (index: number) => {
+      setActiveIndex(index)
+   }
+
+   const category = [
+      { title: 'Главная', url: '#' },
+      { title: 'Уведомление', url: '#' },
+      { title: 'Мессенджер', url: '#' },
+      { title: 'Новости', url: '#' },
+      { title: 'Маркет', url: '#' },
+      { title: 'Видео', url: '#' },
+   ];
+
+   return (
+      <header
+         {...props}
+         className={styles.header}
+      >
+         <div className={styles.row}>
+            <div className={styles.logo}><Link href="/" className="logo"><a><LogoIcon /></a></Link></div>
+            <nav className={styles.menu}>
+               <ul className={styles.list}>
+                  {category.map((item, idx: number) => <li onClick={() => onClickCategory(idx)} className={activeIndex === idx ? styles.active : null} key={idx}><Link href={item.url}><a className={styles.link}>{item.title}</a></Link></li>)}
+               </ul>
+            </nav>
+            <div className={styles.use}>
+               <div className={styles.search}>
+                  <SearchIcon />
+               </div>
+               <div className={styles.user}>
+                  <Link href="/">
+                     <a>
+                        <Image src={UserImage} width={60} height={60} alt="user" />
+                     </a>
+                  </Link>
+               </div>
+            </div>
+         </div>
+
+
+      </header>
+   );
+};
+
+export default Header
